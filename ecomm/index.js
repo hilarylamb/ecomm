c3420d771c03c21cfde2b17b0ed8e7741c02cac4
Hilary Lamb <hilaryklamb@gmail.com>
2020-04-03 17:23:29 -0400

    bodyParser middleware functioning

--- ecomm/index.js
+++ ecomm/index.js
@@ -21,19 +21,25 @@ app.get('/', (req, res) => {
   `);
 });
 
-const bodyParser = () => {
-  req.on('data', data => {
-    const parsed = data.toString('utf8').split('&');
-    const formData = {};
-    for (let pair of parsed) {
-      const [key, value] = pair.split('=');
-      formData[key] = value;
-    }
-    console.log(formData);
-  });
+const bodyParser = (req, res, next) => {
+  if (req.method === 'POST') {
+    req.on('data', data => {
+      const parsed = data.toString('utf8').split('&');
+      const formData = {};
+      for (let pair of parsed) {
+        const [key, value] = pair.split('=');
+        formData[key] = value;
+      }
+      req.body = formData;
+      next();
+    });
+  } else {
+    next();
+  }
 };
 
-app.post('/', (req, res) => {
+app.post('/', bodyParser, (req, res) => {
+  console.log(req.body);
   res.send('Account Created!!!');
 });
 
